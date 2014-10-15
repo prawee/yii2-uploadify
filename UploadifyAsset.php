2c057de447a236fe0977fc9efc32f653813f950a
Prawee Wongsa <prawee@hotmail.com>
2014-10-15 16:43:22 +0700

    updated asset management.

--- UploadifyAsset.php
+++ UploadifyAsset.php
@@ -14,16 +14,28 @@ class UploadifyAsset extends AssetBundle{
     public $sourcePath='@prawee/uploadify/assets';
     
     public $css=[
-        
+        'css/uploadify.css',
     ];
     
-    public $js=[
-        
-    ];
+    public $js=[];
     
     public $depends=[
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
     ];
+    
+    public $publishOptions = ['forceCopy' => YII_DEBUG];
+    
+    private function getJs() {
+        return [
+            YII_DEBUG ? 'jquery.uploadify.js':'jquery.uploadify.min.js',
+        ];
+    }
+    public function init() {
+        if(empty($this->js)){
+            $this->js=$this->getJs();
+        }
+        return parent::init();
+    }
 }
