3fafd796264c99b9dd3fbe5131c1b5455cf91fe3
Prawee Wongsa <prawee@hotmail.com>
2014-10-15 16:15:04 +0700

    changed from AutoloadExample to Uploadify

---
+++ Uploadify.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * 2014-10-15
+ * @author Prawee Wongsa <konkeanweb@gmail.com>
+ */
+namespace prawee\uploadify;
+
+class Uploadify extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
